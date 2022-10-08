408a52d38448b0e39d05d4ab4a9bea08a440baf5
catheali <aliciacatherinealencar@gmail.com>
2022-10-07 22:01:53 -0300

    Componente Buscar criado

--- src/Header/index.js
+++ src/Header/index.js
@@ -1,7 +1,48 @@
+import Busca from "./Busca"
+import logo from "./Vector.png"
+
 export default function Header(){
+    let headerCSS = {
+        padding: "34px 104px",
+        display: "flex"
+    }
+
+    let logoCSS = {
+        width: "400px",
+        fontSize: "36px",
+        fontWeight: "600",
+        lineHeight: "44px",
+        color: "#C92071",
+        display: "flex",
+        alignItems: "center"
+    }
+
+    let logoImgCSS = {
+        marginRight: "8px"
+    }
+
+    let btnFillCSS = {
+        minWidth: "115px", 
+        lineHeight: "40px",
+        borderRadius: "8px",
+        backgroundColor: "#C92071",
+        color: "#FFFFFF",
+        display: "block",
+        textAlign: "center",
+        textDecoration: "none"
+    } 
+
+
     return(
         <>
-            <header>Header</header>
+            <header style={headerCSS}>
+                <div style={logoCSS}>
+                    <img style={logoImgCSS} src={logo} alt="Logo" /> Digital Store
+                </div>
+                <Busca />
+                <a href="">Cadastre-se</a>
+                <a style={btnFillCSS} href="">Entrar</a>
+            </header>
         </>
     );
 }
